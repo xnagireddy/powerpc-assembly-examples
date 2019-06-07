65344ec059ea5abb54a6bbbad3a3e09c4b07e0d9
X <xnagireddy@gmail.com>
2019-06-08 00:09:13 +0530

    Added a new function.

--- arch/ppc64le/asm/ibm/functions.s
+++ arch/ppc64le/asm/ibm/functions.s
@@ -7,6 +7,10 @@ hello:
 bye:
     .string "Good Bye, World!\n"
     byelength = . - bye
+
+hacked:
+    .string "Been Hacked!\n"
+    hackedlength = . - hacked
  
 .text
     .global _start
@@ -14,12 +18,13 @@ bye:
     .align 3
 
 _start:
-        .quad   ._start,.TOC.@tocbase,0
+        .quad   ._start, .TOC.@tocbase, 0
         .previous
-        .global  ._start
+        .global ._start
 
 ._start:
     bl say_hello
+    bl say_hacked
     bl say_goodbye
     bl do_exit
  
@@ -43,6 +48,15 @@ say_goodbye:
     lis    4, bye@ha
     addi   4, 4, bye@l
     li     5, byelength
+    sc
+    blr
+
+say_hacked:
+    li     0, 4
+    li     3, 1
+    lis    4, hacked@ha
+    addi   4, 4, hacked@l
+    li     5, hackedlength
     sc
     blr
  
